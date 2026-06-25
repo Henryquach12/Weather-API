66f4c132e31aa5f29490cd7fb67ff34ed5ca6299
Kien Hao Quach <a1942629@adelaide.edu.au>
2026-06-25 16:00:23 +0930

    Implement weather data retrieval and error handling in get_weather endpoint

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 app = FastAPI()
 API_KEY = os.getenv("WEATHER_API_KEY")
+
+
 @app.get("/weather/{city}")
 def get_weather(city: str):
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations={city},VA,20170&aggregateHours=24&forecastDays=5&unitGroup=us&shortColumnNames=false&contentType=json&key={API_KEY}"
+    try: 
+        response = requests.get(url)
+    except Exception:
+        raise HTTPException(status_code = 500, detail="Cannot connect to weather API")
+    
+    if response.status_code == 400:
+        raise HTTPException(status_code=400, detail="City is not valid")
+    elif response.status_code == 401:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+    elif response.status_code == 429:
+        raise HTTPException(status_code=429, detail="Too many requests")
+    elif response.status_code >= 500:
+        raise HTTPException(status_code=503, detail="Weather API is unavailable")
+
+    data = response.json()
+    location_key = list(data["locations"].keys())[0]
+    today = data["locations"][location_key]["values"][0]
+    
     return {
         "city": city,
-        "temperature": 32, 
-        "humidity": 80,
-        "conditions": "Partly cloudy"
+        "city": city,
+        "temperature": today["temp"],
+        "humidity": today["humidity"],
+        "conditions": today["conditions"],
+        "min_temp": today["mint"],
+        "max_temp": today["maxt"],
+        "date": today["datetimeStr"]
     }
+    
